b8da22a817a311f0e046c90a65408708b8d0c7b0
VictorG-028 <victor6g0@gmail.com>
2022-03-28 18:36:01 -0300

    Primeiro Commit

---
+++ src/test/java/br/personal/CalculadoraCientificaRestfull/CalculadoraCientificaRestfullApplicationTests.java
@@ -0,0 +1,13 @@
+package br.personal.CalculadoraCientificaRestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraCientificaRestfullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
